6a73a6b36d900d7eddadcb6bf310fc591b735045
Ben <ben@vivotek.com>
2022-10-23 17:08:06 +0800

    add OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
-
+#include <ArduinoOTA.h>
+#include <ESP8266WiFi.h>
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  /**
+   * Connet to Wifi
+   */
+  WiFi.begin("bcs", "28911603");
+  uint32_t notConnectedCounter = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(100);
+      Serial.println("Wifi connecting...");
+      notConnectedCounter++;
+      if(notConnectedCounter > 150) { // Reset board if not connected after 5s
+          Serial.println("Resetting due to Wifi not connecting...");
+          ESP.restart();
+      }
+  }
+  Serial.print("Wifi connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  /**
+   * Enable OTA update
+   */
+  ArduinoOTA.setHostname("esp");
+  ArduinoOTA.begin();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Check for over the air update request and (if present) flash it
+  ArduinoOTA.handle();
 }
